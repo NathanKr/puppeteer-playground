569e12f27965c4b913a383c6ed6449abf882b31e
Nathan Krasney <natankrasney@gmail.com>
2021-04-06 10:55:58 +0300

    use iframe

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ console.log("app has started ...");
 const operation = process.argv[2];
 
 switch(operation){
+  case "page-with-iframe" :
+    require('./operations/page-with-iframe').run();
+    break
+
   case "page-to-image" :
   require('./operations/page-to-image').run();
   break
---
+++ operations/page-with-iframe.js
@@ -0,0 +1,39 @@
+const url = require("url");
+const path = require("path");
+
+const getUriLocalHtmlFile = () => {
+  const fullpathIndexHtml = path.join(__dirname, "client", "index-with-iframe.html");
+  // --- following is required only because the file is local
+  // --- if your page is accessed by browser you can use http and not file
+  const uriLocalHtmlFile = url.pathToFileURL(fullpathIndexHtml).href;
+  console.log(uriLocalHtmlFile);
+
+  return uriLocalHtmlFile;
+};
+
+async function run() {
+  const puppeteer = require("puppeteer");
+  // --- open browser and sleep 100 ms between operations
+  const browser = await puppeteer.launch({ headless: true, slowMo: 100 });
+  const page = await browser.newPage();
+
+  await page.goto(getUriLocalHtmlFile());
+  console.log("browser is opened with local file");
+
+
+  // ******** now we are on the page   *********
+  val = await page.$eval("body", elem => elem.innerHTML);
+  console.log(`*********** body in page\n ${val}`);
+
+
+  const frameHandle = await page.$("#iframe1");
+  // console.log(frameHandle);
+  const frame = await frameHandle.contentFrame();
+  // console.log(frame);
+  val = await frame.$eval("body", elem => elem.innerHTML);
+  console.log(`------------ body in iframe : ${val}`);
+
+  await browser.close();
+}
+
+module.exports = { run };
