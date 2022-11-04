004be3fb0b405b4f6d1b7265f1a8254db5b6ae58
Nathan Krasney <natankrasney@gmail.com>
2022-11-04 11:46:15 +0200

    change to working url

--- operations/page-to-image.js
+++ operations/page-to-image.js
@@ -4,8 +4,8 @@ async function run() {
   const pngFile = "example.png";
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  // await page.goto("https://example.com");
-  await page.goto("https://www.411.com/");
+  await page.goto("https://example.com");
+  // await page.goto("https://www.411.com/");
 
   await page.screenshot({ path: pngFile });
   console.log(`pdf is written to file : ${pngFile}`);
