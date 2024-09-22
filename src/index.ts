1c87c94275a331ca41a3408133b55b3441f83b0e
nathankr <natankrasney@gmail.com>
2024-09-22 12:40:51 +0300

    better index file

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,12 @@
-// console.log("app has started ...");
-import { getOperationUrl } from './utils';
+console.log("app has started ...");
+import { getOperationUrl } from "./utils";
 
 const operation = process.argv[2]; // e.g. page-to-image
 
-switch(operation) {
-  case operation:
-    import(getOperationUrl(operation)).then(module => {
-      module.run();
-    }).catch(err => {
-      console.error(`Failed to load module: ${err}`);
-    });
-    break;
-
-  default:
-    console.error(`unknown operation : ${operation}`);
-}
+import(getOperationUrl(operation))
+  .then((module) => {
+    module.run();
+  })
+  .catch((err) => {
+    console.error(`Failed to load module: ${err}`);
+  });
