1e2f76684d84e30c24e055d519302c2f9c9e37fd
nathankr <natankrasney@gmail.com>
2024-11-20 12:15:40 +0200

    add and use areElementsEqual

--- src/operations/element-handle.ts
+++ src/operations/element-handle.ts
@@ -1,5 +1,14 @@
-import puppeteer, { ElementHandle } from "puppeteer";
+import puppeteer, { ElementHandle, Page } from "puppeteer";
 import { getUriLocalHtmlFileInDataDir } from "../utils";
+
+async function areElementsEqual(
+  page: Page,
+  element1: ElementHandle<Element>,
+  element2: ElementHandle<Element>
+): Promise<boolean> {
+  const equal = await page.evaluate((e1, e2) => e1 === e2, element1, element2);
+  return equal;
+}
 
 export async function run(): Promise<void> {
   const browser = await puppeteer.launch();
@@ -9,6 +18,15 @@ export async function run(): Promise<void> {
   /* Get ElementHandles for all elements
    i am using page.$$ because you can not return Element[] from page.evaluate */
   const allElementHandles: ElementHandle<Element>[] = await page.$$("body *");
+  const allElementHandles1: ElementHandle<Element>[] = await page.$$("body *");
+  // -- simple == will not do
+  const equal = await areElementsEqual(
+    page,
+    allElementHandles[0],
+    allElementHandles1[0]
+  );
+  console.log(equal);
+
   console.log(`Found ${allElementHandles.length} elements in body`);
   // Example: Log the id of each element
   for (const elementHandle of allElementHandles) {
@@ -16,11 +34,10 @@ export async function run(): Promise<void> {
     console.log(`id : ${id}`);
     const rect = await elementHandle.evaluate((el) => {
       // CAUTION : return rect will return empty DOM object for some reason
-      const rect = el.getBoundingClientRect(); 
-      return {top : rect.top , height : rect.height} // this is ok
-    } );   
+      const rect = el.getBoundingClientRect();
+      return { top: rect.top, height: rect.height }; // this is ok
+    });
     console.log(rect);
-     
   }
   await browser.close();
 }
