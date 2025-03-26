3b8e404bd04a0ae69cf2b454877154f140929998
nathankr <natankrasney@gmail.com>
2025-03-26 09:27:01 +0200

    no-sandbox try

--- src/operations/page-to-image.ts
+++ src/operations/page-to-image.ts
@@ -5,7 +5,12 @@ import {getOutputDirPath} from '../utils'
 const pngFile = "example.png";
 
 export async function run() {
-  const browser = await puppeteer.launch({ headless: true }); // Use true, false, or "shell"
+  // const browser = await puppeteer.launch({ headless: true }); // Use true, false, or "shell"
+  // Add the --no-sandbox flag here
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox'] // Including the flag in the args array
+  });
   const page = await browser.newPage();
   await page.goto("https://example.com");
   // await page.goto("https://www.411.com/");
