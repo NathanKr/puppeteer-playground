2b90abb546046d56066209f21b0e7a3a24985905
nathankr <natankrasney@gmail.com>
2025-04-07 12:45:53 +0300

    full page

--- src/operations/page-to-image.ts
+++ src/operations/page-to-image.ts
@@ -8,12 +8,13 @@ export async function run() {
   const browser = await puppeteer.launch({ headless: true }); // Use true, false, or "shell"
  
   const page = await browser.newPage();
-  await page.goto("https://example.com");
+  await page.goto("https://www.post2youtube.com");
+  // await page.goto("https://example.com");
   // await page.goto("https://www.411.com/");
 
   const filePath = path.resolve(getOutputDirPath(), pngFile);
 
-  await page.screenshot({ path: filePath });
+  await page.screenshot({ path: filePath , fullPage: true});
   console.log(`Screenshot is written to file: ${filePath}`);
 
   await browser.close();
