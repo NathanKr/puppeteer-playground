d396c84319a27ab79208b6978906c317eb3d90c8
nathankr <natankrasney@gmail.com>
2025-12-23 04:55:37 +0200

    use vercel post

--- src/operations/page-to-image.ts
+++ src/operations/page-to-image.ts
@@ -8,7 +8,10 @@ export async function run() {
   const browser = await puppeteer.launch({ headless: true }); // Use true, false, or "shell"
  
   const page = await browser.newPage();
-  await page.goto("https://www.post2youtube.com");
+  await page.goto("https://vercel.com/blog/self-driving-infrastructure");
+  // await page.goto("https://vercel.com/kb/bulletin/react2shell");
+  // await page.goto("https://blog.apify.com/how-to-scrape-website");
+  // await page.goto("https://www.post2video.com/privacy-policy");
   // await page.goto("https://example.com");
   // await page.goto("https://www.411.com/");
 
